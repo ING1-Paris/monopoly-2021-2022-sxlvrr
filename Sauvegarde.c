1a474d251eae19ba118c4b88d5305b3d8d6b162b
johngorden <96013055+johngorden@users.noreply.github.com>
2021-12-17 11:28:07 +0100

    Update and rename Sauvegarde to Sauvegarde.c

---
+++ Sauvegarde.c
@@ -0,0 +1,7 @@
+"""create new file"""
+
+def sauvegarder_une_partie(fichier_sauvegarde){
+  maSauvegarde=fopen("sauvegarde","w+");
+}
+def charger_une_partie(){
+  maSauvegarde=fopen("sauvegarde","a+");
