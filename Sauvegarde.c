f4252464822d388882358b0f799f13e6fd72b723
OscarHECE <71657314+OscarNsi@users.noreply.github.com>
2021-12-22 16:17:47 +0100

    Update Sauvegarde.c

--- Sauvegarde.c
+++ Sauvegarde.c
@@ -4,7 +4,7 @@ int sauvegarder_une_partie(fichier_sauvegarde){
   maSauvegarde=fopen("game.txt", "a+");
  
   if(maSauvegarde == NULL) {
-        printf("file can't be opened\n");
+        printf("NE peux pas etre ouvert\n");
         exit(1);
   } 
   fprintf(maSauvegarde, "%d,", data);
