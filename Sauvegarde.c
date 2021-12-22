d5421d8f533ef75b24ed39b6c9df64235ca3549e
johngorden <96013055+johngorden@users.noreply.github.com>
2021-12-22 21:11:01 +0800

    Update Sauvegarde.c

--- Sauvegarde.c
+++ Sauvegarde.c
@@ -1,7 +1,19 @@
 """create new file"""
 
 def sauvegarder_une_partie(fichier_sauvegarde){
-  maSauvegarde=fopen("sauvegarde","w+");
+  maSauvegarde=fopen("game.txt", "a+");
+ 
+  if(maSauvegarde == NULL) {
+        printf("file can't be opened\n");
+        exit(1);
+  } 
+  fprintf(maSauvegarde, "%d,", data);
+  fclose(maSauvegarde);
 }
+
+
 def charger_une_partie(){
   maSauvegarde=fopen("sauvegarde","a+");
+  fprintf(maSauvegarde, "%d,", data);
+  fclose(maSauvegarde);
+}
