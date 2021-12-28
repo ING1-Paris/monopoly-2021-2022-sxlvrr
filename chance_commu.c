054a601261144969e4efd9e679eb73dfde35ff82
sxlvrr <thesylverofficial@gmail.com>
2021-12-28 17:24:23 +0100

    update commuchance

--- chance_commu.c
+++ chance_commu.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "carte.h"
 
 void luckcard()
 {
@@ -14,5 +15,31 @@ void luckcard()
     if (r==1){
         printf("Vous sortez de prison.\nVous pouvez conserver cette carte jusqu'a ce qu'elle soit utilisee ou vendue.");
 
+
+    }
+}
+
+void commucard()
+{
+    srand(time(NULL));
+    int r;
+    char description;
+    printf("Carte communaute !\n");
+    r=rand() % 21;
+
+    if (r==1){
+        printf("Oeuvre de charite\nLa croix rouge recolte des dons et vous etes favorable a leurs actions.\nVersez 100€ a la banque.");
+        if(j1 == tableau[17]){
+            p1.argent -=100;
+        }
+        if(j2 == tableau[17]){
+            p2.argent -=100;
+        }
+        if(j3 == tableau[17]){
+            p3.argent -=100;
+        }
+        if(j4 == tableau[17]){
+            p4.argent -=100;
+        }
     }
 }
