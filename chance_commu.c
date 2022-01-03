22b1134df729a5add42a9eeb324fa454af8bb9c4
sxlvrr <thesylverofficial@gmail.com>
2022-01-03 08:47:48 +0100

    Update chance_commu.c

--- chance_commu.c
+++ chance_commu.c
@@ -49,16 +49,19 @@ void commucard()
             p3.argent -=200;
             p4.argent -=200;
             p1.argent +=600;
+        }
         if(j2 == tableau[17]){
             p1.argent -=200;
             p3.argent -=200;
             p4.argent -=200;
             p2.argent +=600;
+        }
         if(j3 == tableau[17]){
             p1.argent -=200;
             p2.argent -=200;
             p4.argent -=200;
             p3.argent +=600;
+        }
         if(j4 == tableau[17]){
             p1.argent -=200;
             p2.argent -=200;
@@ -67,16 +70,19 @@ void commucard()
         }
     }
     if (r==3){
-        if(j1 == tableau[17] & p1.argent >= 750000){
+        if(j1 == tableau[17] && p1.argent >= 750000){
             printf("Impot sur la fortune\nVotre richesse depassant la moyenne, vous vous devez de verser 10 pourcents de cette dernière a la banque");
             p1.argent -= (p1.argent/100)*10;
-        if(j2 == tableau[17] & p2.argent >= 750000){
+        }
+        if(j2 == tableau[17] && p2.argent >= 750000){
             printf("Impot sur la fortune\nVotre richesse depassant la moyenne, vous vous devez de verser 10 pourcents de cette dernière a la banque");
             p2.argent -= (p2.argent/100)*10;
-        if(j3 == tableau[17] & p3.argent >= 750000){
+        }
+        if(j3 == tableau[17] && p3.argent >= 750000){
             printf("Impot sur la fortune\nVotre richesse depassant la moyenne, vous vous devez de verser 10 pourcents de cette dernière a la banque");
             p3.argent -= (p3.argent/100)*10;
-        if(j4 == tableau[17] & p4.argent >= 750000){
+        }
+        if(j4 == tableau[17] && p4.argent >= 750000){
             printf("Impot sur la fortune\nVotre richesse depassant la moyenne, vous vous devez de verser 10 pourcents de cette dernière a la banque");
             p4.argent -= (p4.argent/100)*10;
         }
@@ -84,6 +90,8 @@ void commucard()
             r=rand() % 21;
         }
     }
-    
+    if (r==4){
+        printf("")
+    }
 
 }
