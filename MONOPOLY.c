6dd8478162fa513b03818989e7cbf8bde9f5ac92
LexouilleInfo <alexandre@danoffre.fr>
2022-01-03 15:02:36 +0100

    fjiuahfiue

---
+++ MONOPOLY.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "plateau.h"
+#include "carte.h"
+#include "tabplateau.h"
+//#include "displayCarte.h"
+int main(){
+    
+plateau();
+carte();
+
+}
