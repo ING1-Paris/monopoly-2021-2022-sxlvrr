37d42975e45c888006554117c1815ab0907fbd20
sxlvrr <thesylverofficial@gmail.com>
2022-01-04 15:29:16 +0100

    etc (KL)

--- biblio.h
+++ biblio.h
@@ -1,6 +1,14 @@
 #ifndef BIBLI
 #define BIBLI
 
+typedef struct t_joueur{
+    char nom[20];
+    int numero;
+    int argent;
+    int position;
+    int possessions[30];
+}joueur;
+
 int my_strlen(const char *str);
 
 void nouvellePartie(void);
