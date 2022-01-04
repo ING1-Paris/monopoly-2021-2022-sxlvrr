b8e739770c15ef23ce6c08c612722d0516c03246
sxlvrr <thesylverofficial@gmail.com>
2022-01-04 19:24:40 +0100

    Update biblio.h

--- biblio.h
+++ biblio.h
@@ -1,4 +1,4 @@
-#ifndef BIBLI
+//#ifndef BIBLI
 #define BIBLI
 #include <Windows.h>
 #include "Color.c"
@@ -10,8 +10,30 @@ typedef struct t_joueur{
     int numero;
     int argent;
     int position;
+    int enprison;
     int possessions[30];
 }joueur;
+
+joueur *initplayers(){
+    joueur *p = (joueur *)malloc(4*sizeof(joueur));
+    
+    joueur j1 = {"", 1, 0, 0, 0, NULL};
+    p[0] = j1;
+    joueur j2 = {"", 2, 0, 0, 0, NULL};
+    p[1] = j2;
+    joueur j3 = {"", 3, 0, 0, 0, NULL};
+    p[2] = j3;
+    joueur j4 = {"", 4, 0, 0, 0, NULL};
+    p[3] = j4;
+    //strcpy(p[1].possessions, "")
+
+    return p;
+}
+
+int main() {
+    joueur *p = initplayers();
+    //*p = luckcard(p);
+}
 
 int my_strlen(const char *str);
 
