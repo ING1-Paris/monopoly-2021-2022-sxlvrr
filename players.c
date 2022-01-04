7224e092b8f5d56aec1e3d5ee61fdbc68a95f2b2
sxlvrr <thesylverofficial@gmail.com>
2022-01-04 19:17:36 +0100

    Update players.c

--- players.c
+++ players.c
@@ -7,19 +7,20 @@ typedef struct t_joueur{
     int numero;
     int argent;
     int position;
+    int enprison;
     int possessions[30];
 }joueur;
 
 joueur *initplayers(){
     joueur *p = (joueur *)malloc(4*sizeof(joueur));
     
-    joueur j1 = {"", 1, 0, 0, NULL};
+    joueur j1 = {"", 1, 0, 0, 0, NULL};
     p[0] = j1;
-    joueur j2 = {"", 2, 0, 0, NULL};
+    joueur j2 = {"", 2, 0, 0, 0, NULL};
     p[1] = j2;
-    joueur j3 = {"", 3, 0, 0, NULL};
+    joueur j3 = {"", 3, 0, 0, 0, NULL};
     p[2] = j3;
-    joueur j4 = {"", 4, 0, 0, NULL};
+    joueur j4 = {"", 4, 0, 0, 0, NULL};
     p[3] = j4;
     //strcpy(p[1].possessions, "")
 
@@ -28,5 +29,5 @@ joueur *initplayers(){
 
 int main() {
     joueur *p = initplayers();
-    *p = luckcard(p);
+    //*p = luckcard(p);
 }
