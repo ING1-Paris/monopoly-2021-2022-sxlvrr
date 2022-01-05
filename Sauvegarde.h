4984074091cdf21f0a6ce91768443b6461027086
sxlvrr <thesylverofficial@gmail.com>
2022-01-05 22:49:31 +0100

    Delete Sauvegarde.h

--- Sauvegarde.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-  typedef struct{
-    char nom;
-    int position, monnaie;
-    float propriete;
-  }Joueur;
-Sauvegarde(fichierdesauvegarde){};
-Chargement(fichierdesauvegarde){};
