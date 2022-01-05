6884daa0f015608cc9b9540239338d2380cc93e1
sxlvrr <thesylverofficial@gmail.com>
2022-01-05 15:43:48 +0100

    Update Position.c

--- Position.c
+++ Position.c
@@ -4,11 +4,11 @@
 
 int main()
 {
-    int i = 1, de = 0, nb = 0, lance = 0, fin = 0;
+    int i = 1, de = 0, nb = 0, lance = 0, fin = 0, prison = 0;
 
     while(nb <=1 || nb >= 5){
-    printf("Combien de joueurs ?\n");
-    scanf("%d",&nb);
+        printf("Combien de joueurs ?\n");
+        scanf("%d",&nb);
     }
 
     int d[nb];
@@ -28,29 +28,40 @@ int main()
                 i=1;
             }
             while(lance!=1){
-                printf("Joueur %d, c'est a toi ! (tape 1 pour lancer le de)\n", i);
+                if(d[i]!=25){
+                    printf("Joueur %d, c'est a toi ! (tape 1 pour lancer le de)\n", i);
+                }
+                else{
+                    printf("Joueur %d, tu es en prison, fait un 6 en lancant le de (en tapant 1) pour sortir !\n", i);
+                }
                 scanf("%d", &lance);
             }
         }
 
         if (lance == 1){
-            while(d[i] == 25 && de !=6){ //Si le joueur est en prison, un dé de 6 lui permettra de sortir
+            if(d[i] == 25){ //Si le joueur est en prison, un dé de 6 lui permettra de sortir
+                prison = 1;
                 srand(time(NULL));
                 de = rand() %6+1;
+                printf("%d",de);
+                lance = 0;
+                if(de == 6){
+                    d[i] += 1;
+                }
             }
-
-            srand(time(NULL));
-            de = rand() %6+1;
-            d[i] += de;
-            if(d[i] >=30){
-                d[i] -= 30;
+            else{
+                if(prison == 1){
+                    d[i] -= 1;
+                }
+                srand(time(NULL));
+                de = rand() %6+1;
+                d[i] += de;
+                if(d[i] >=30){
+                    d[i] -= 30;
+                }
+                printf("%4d\n", d[i]);
+                lance = 0;
             }
-            printf("%4d\n", d[i]);
-            lance = 0;
-        }
-
-        if (d[i] == 25){
-            
         }
     }while(fin==0);
 
